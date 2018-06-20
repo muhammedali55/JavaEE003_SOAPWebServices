2b40a84c5ae288e11d8ee53c36f3d5e296f494f3
muhammet <muhammet@DESKTOP-HCSILBU>
2018-06-20 10:27:09 +0300

    ilk yükleme

---
+++ src/com/vektorel/soapwebservices/orneksoap.java
@@ -0,0 +1,17 @@
+package com.vektorel.soapwebservices;
+
+public class orneksoap {
+
+	public String kelimebirlestir(String yaziA, String yaziB) {
+		
+		// Buraya istediğiniz işlemeri ve kodları yapınız
+		
+		return yaziA+" "+yaziB;
+	}
+	
+	public int toplama(int a, int b) {
+		
+		return a+b;
+	}
+	
+}
